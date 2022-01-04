3160f3d2d7afc60dd723f6260995f3c66a59db1e
ESGDeveloper <esgdeveloper4u@gmail.com>
2022-01-04 22:27:46 +0900

    binary: resolved Counting Bits

---
+++ Binary/countingBits.js
@@ -0,0 +1,10 @@
+/* 338. Counting Bits
+ * Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), ans[i] is the number of 1's in the binary representation of i.
+*/
+const countBits = (n) => {
+    let returnArr = [];
+    for (let i = 0; i <= n; ++i) {
+        returnArr.push(i.toString(2).split('').filter(x => x === '1').length);
+    }
+    return returnArr;
+};
